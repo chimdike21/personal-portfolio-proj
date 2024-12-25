a1e740a76ab1c7640b713d94b809b9a073e27716
chimdike21 <nwachukwudivine02@gmail.com>
2024-12-25 12:26:18 +0100

    works

--- script.js
+++ script.js
@@ -6,8 +6,25 @@ document.addEventListener("DOMContentLoaded", function(){
             if (entry.isIntersecting){
                 skillLogo.classList.add('visible');
                 observer.unobserve(skillLogo)
-            }
+            } 
         })
     })
     observer.observe(skillLogo);
+})
+
+document.addEventListener("DOMContentLoaded", function(){
+    const h1Element= document.querySelector("#contact h1");
+    function checkVisibility(){
+        const rect= h1Element.getBoundingClientRect();
+        const windowHeight= window.innerHeight;
+
+        if(rect.top <= windowHeight && rect.bottom >= 0){
+            h1Element.classList.add("visible"); //adds the visible class
+            
+        } else {
+            h1Element.classList.remove("visible")
+        }
+    }
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility()
 })
